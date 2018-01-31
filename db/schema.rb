8cf6c638550806b2a708524b5dece2183d88df0c
Quentin <quent.guillaume@gmail.com>
2018-01-30 18:37:59 -0800

    comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131021157) do
+ActiveRecord::Schema.define(version: 20180131023310) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "photo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string "name"
