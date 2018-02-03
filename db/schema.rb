29ff2620c643b6580fcb0d3616e584d1054baca5
Quentin <quent.guillaume@gmail.com>
2018-02-03 10:07:09 -0800

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131214848) do
+ActiveRecord::Schema.define(version: 20180131024551) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20180131214848) do
     t.string "name"
     t.text "description"
     t.string "picture"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
